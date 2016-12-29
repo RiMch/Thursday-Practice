716638c299ae29c3ff93f402361951be8bce2aea
Rich <rich5671@verizon.net>
2016-12-29 12:23:10 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/rich/projectthur1209pm/Thur1214PM.java
@@ -0,0 +1,13 @@
+package com.example.rich.projectthur1209pm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Thur1214PM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thur1214_pm);
+    }
+}
